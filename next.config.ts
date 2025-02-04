d87ef7d635b8d8f44b2eea8ebb700395b302ae97
decctor <ampereprocessos@gmail.com>
2025-02-04 14:31:16 -0300

    new configuration for next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "firebasestorage.googleapis.com",
+			},
+			{
+				protocol: "https",
+				hostname: "sc-erp.s3.amazonaws.com",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+		],
+	},
+	/* config options here */
 };
 
 export default nextConfig;
